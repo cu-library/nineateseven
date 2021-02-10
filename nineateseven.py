5d8983e351d0dd225dc53ac1df2fbf1c310fbad5
Kevin Bowrin <kevin.bowrin@carleton.ca>
2021-02-09 19:29:34 -0500

    Databases and taxonomy terms now migrate

--- nineateseven.py
+++ nineateseven.py
@@ -8,6 +8,8 @@ import json
 import requests
 import pprint
 import configparser
+import copy
+import re
 
 CONTEXT_SETTINGS = {"auto_envvar_prefix": "NINEATESEVEN"}
 
@@ -20,7 +22,7 @@ CONTEXT_SETTINGS = {"auto_envvar_prefix": "NINEATESEVEN"}
 @click.option("--target", "-t", required=True)
 @click.option("--targetusername", "-tu", required=True)
 @click.option("--targetpassword", "-tp", required=True)
-@click.option("--fieldmappings", "-fm", type=click.File(), required=True)
+@click.option("--config", "-c", type=click.File(), required=True)
 @click.option(
     "--disablesubjectaltnamewarning/--no-disablesubjectaltnamewarning", default=False
 )
@@ -33,19 +35,20 @@ def cli(
     target,
     targetusername,
     targetpassword,
-    fieldmappings,
+    config,
     disablesubjectaltnamewarning,
     bundles,
 ):
     """Migrate a Drupal 7 node bundle to Drupal 9."""
     click.echo("Starting migration...")
+    click.echo("")
     click.echo(f"    Database name: {db}")
     click.echo(f" Database charset: {dbcharset}")
     click.echo(f"    Database user: {dbusername}")
     click.echo(f"  Target JSON API: {target}")
     click.echo("")
 
-    # Connect to the local MySQL database
+    # Connect to the local MySQL database.
     connection = pymysql.connect(
         host="localhost",
         database=db,
@@ -55,67 +58,118 @@ def cli(
         cursorclass=pymysql.cursors.DictCursor,
     )
 
-    # Pesky security warning particular to Carleton's environment
+    # Pesky security warning particular to Carleton's environment.
     if disablesubjectaltnamewarning:
         import warnings
         import urllib3
 
         warnings.simplefilter("ignore", urllib3.exceptions.SubjectAltNameWarning)
 
-    # Read in the config file
-    config = configparser.ConfigParser()
-    config.read_file(fieldmappings)
+    # Read in the config file.
+    cp = configparser.ConfigParser(interpolation=None)
+    cp.read_file(config)
 
-    # Global nid to d9node map
-    nid_to_d9node = {}
+    # Mapping from D7 NIDs to newly created D9 nodes.
+    nid_to_d9_node = {}
 
-    # All fields in D7 DB
-    fields = get_fields(connection)
+    # Mapping from D7 TIDs to newly created D9 taxonomy terms.
+    tid_to_d9_taxonomy_term = {}
 
+    # Context manager around the connection, so it will be automatically closed.
     with connection:
+        # First, migrate the taxonomy terms.
+        # We migrate them first, because nodes might reference them.
+        # Iterate through the D7 machine names for taxonomy vocabularies and their
+        # corresponding D9 machine names
+        for d7_taxonomy_voc, d9_taxonomy_voc in cp.items("taxonomy_mappings"):
+            terms = create_taxonomy_terms(connection, d7_taxonomy_voc, d9_taxonomy_voc)
+            with click.progressbar(
+                length=len(terms),
+                label=f"POSTing taxonomy terms in {d7_taxonomy_voc}",
+            ) as bar:
+                # Iterate through a copy of the terms, processing terms
+                # which are at the root ("0" parent) or have a parent which
+                # is already processed.
+                # After processing a term, delete it from the terms list.
+                # Loop this process until all terms in the terms list have
+                # been processed.
+                while len(terms) > 0:
+                    this_pass_terms = terms.copy()
+                    for term in this_pass_terms:
+                        if (
+                            term["parent_tid"] == 0
+                            or term["parent_tid"] in tid_to_d9_taxonomy_term
+                        ):
+                            if term["parent_tid"] in tid_to_d9_taxonomy_term:
+                                term["data"]["relationships"]["parent"]["data"][
+                                    "id"
+                                ] = tid_to_d9_taxonomy_term[term["parent_tid"]]["data"][
+                                    "id"
+                                ]
+                            tid = term["tid"]
+                            # Delete tid and parent_tid from data to be POSTed.
+                            del term["tid"]
+                            del term["parent_tid"]
+                            tid_to_d9_taxonomy_term[tid] = post(
+                                term,
+                                "taxonomy_term",
+                                d9_taxonomy_voc,
+                                target,
+                                targetusername,
+                                targetpassword,
+                            )
+                            terms.remove(term)
+                            bar.update(1)
+
+        # Get field configs in D7 DB
+        fields = get_fields(connection)
+
         for bundle in bundles:
-            count = count_nodes_of_bundle(connection, bundle)
-            click.echo(f"{count} nodes of type {bundle}")
             nodes = create_nodes(connection, bundle)
-            with click.progressbar(nodes) as bar:
+            with click.progressbar(
+                nodes,
+                label=f"POSTing {bundle} nodes to target",
+                item_show_func=lambda node: str(node["nid"])
+                if node is not None
+                else "",
+            ) as bar:
                 for node in bar:
                     nid = node["nid"]
+                    node = copy.deepcopy(
+                        node
+                    )  # Progress bar can still use original node
                     del node["nid"]
-                    nid_to_d9node[nid] = post_node(
-                        node, bundle, target, targetusername, targetpassword
+                    nid_to_d9_node[nid] = post(
+                        node, "node", bundle, target, targetusername, targetpassword
                     )
 
-        for d7fieldname, d9fieldname in config.items("fieldmappings"):
-            click.echo(f"PATCHing {d7fieldname}")
-            with click.progressbar(length=len(nid_to_d9node)) as bar:
-                for nid, d9node in nid_to_d9node.items():
-                    field_config = fields[d7fieldname]
+        for d7_fieldname, d9_fieldname in cp.items("field_mappings"):
+            with click.progressbar(
+                nid_to_d9_node,
+                label=f"PATCHing {d7_fieldname}",
+                item_show_func=lambda nid: str(nid) if nid is not None else "",
+            ) as bar:
+                for nid in bar:
+                    field_config = fields[d7_fieldname]
                     field = create_field(
+                        cp,
                         connection,
                         bundle,
                         nid,
-                        d9node["data"]["id"],
-                        nid_to_d9node,
-                        d7fieldname,
-                        d9fieldname,
+                        nid_to_d9_node,
+                        tid_to_d9_taxonomy_term,
+                        d7_fieldname,
+                        d9_fieldname,
                         field_config,
                     )
-                    patch_field(
+                    patch(
                         field,
+                        "node",
                         bundle,
                         target,
                         targetusername,
                         targetpassword,
                     )
-                    bar.update(1)
-
-
-def count_nodes_of_bundle(connection, bundle):
-    with connection.cursor() as cursor:
-        sql = "SELECT COUNT(*) AS count FROM `node` WHERE `type`=%s"
-        cursor.execute(sql, (bundle,))
-        result = cursor.fetchone()
-        return result["count"]
 
 
 def get_fields(connection):
@@ -126,6 +180,44 @@ def get_fields(connection):
         for row in cursor:
             fields[row["field_name"]] = {"type": row["type"], "module": row["module"]}
     return fields
+
+
+def create_taxonomy_terms(connection, d7_machine_name, d9_machine_name):
+    terms = []
+    with connection.cursor() as cursor:
+        sql = (
+            "SELECT `taxonomy_term_data`.`tid`, "
+            "       `taxonomy_term_data`.`name`, "
+            "       `taxonomy_term_hierarchy`.`parent` "
+            "FROM `taxonomy_vocabulary` "
+            "LEFT JOIN `taxonomy_term_data` "
+            "    ON `taxonomy_vocabulary`.`vid` = `taxonomy_term_data`.`vid` "
+            "LEFT JOIN `taxonomy_term_hierarchy` "
+            "    ON `taxonomy_term_data`.`tid` = `taxonomy_term_hierarchy`.`tid` "
+            "WHERE `taxonomy_vocabulary`.`machine_name` = %s"
+        )
+        cursor.execute(sql, (d7_machine_name,))
+        for row in cursor:
+            term = {
+                "tid": row["tid"],
+                "parent_tid": row["parent"],
+                "data": {
+                    "type": f"taxonomy_vocabulary--{d9_machine_name}",
+                    "attributes": {},
+                    "relationships": {
+                        "parent": {
+                            "data": {
+                                "type": f"taxonomy_term--{d9_machine_name}",
+                                "id": "virtual",
+                            },
+                        },
+                    },
+                },
+            }
+            term["data"]["attributes"]["langcode"] = "en"
+            term["data"]["attributes"]["name"] = row["name"].strip()
+            terms.append(term)
+    return terms
 
 
 def create_nodes(connection, bundle):
@@ -151,53 +243,124 @@ def create_nodes(connection, bundle):
 
 
 def create_field(
-    connection, bundle, nid, d9id, nid_to_d9node, d7fieldname, d9fieldname, field_config
+    config,
+    connection,
+    bundle,
+    nid,
+    nid_to_d9_node,
+    tid_to_d9_taxonomy_term,
+    d7fieldname,
+    d9fieldname,
+    field_config,
 ):
-    field = {"data": {"type": f"node--{bundle}", "id": d9id, "attributes": {}}}
+    field = {
+        "data": {
+            "type": f"node--{bundle}",
+            "id": nid_to_d9_node[nid]["data"]["id"],
+            "attributes": {},
+            "relationships": {},
+        }
+    }
     with connection.cursor() as cursor:
         sql = (
             f"SELECT * FROM `field_data_{d7fieldname}` "
-            "WHERE `entity_type`='node' AND bundle=%s AND entity_id=%s"
+            "WHERE `entity_type` = 'node' AND `bundle` = %s AND `entity_id` = %s "
+            "ORDER BY `delta`"
         )
         cursor.execute(sql, (bundle, nid))
-        fields_data = []
+        attributes = []
+        relationships = []
         for row in cursor:
-            field_data = {}
             if field_config["type"] == "link_field":
-                try:
-                    field_data["uri"] = clean_uri(
-                        row[f"{d7fieldname}_url"], nid_to_d9node
-                    )
-                except KeyError:
-                    click.echo("----")
-                    click.echo(nid)
-                    click.echo("----")
+                field_data = {}
+                field_data["uri"] = clean_uri(row[f"{d7fieldname}_url"], nid_to_d9_node)
                 field_data["title"] = row[f"{d7fieldname}_title"]
-            fields_data.append(field_data)
-        if fields_data:
-            field["data"]["attributes"][d9fieldname] = fields_data
+                attributes.append(field_data)
+            elif field_config["type"] == "text" or field_config["type"] == "text_long":
+                # If the d7 text field has a null format, the d9 text field is plain.
+                if row[f"{d7fieldname}_format"] is None:
+                    attributes.append(row[f"{d7fieldname}_value"])
+                else:
+                    field_data = {}
+                    field_data["value"] = clean_text(
+                        row[f"{d7fieldname}_value"], nid_to_d9_node
+                    )
+                    field_data["format"] = config["text_format_mappings"][
+                        str(row[f"{d7fieldname}_format"])
+                    ]
+                    attributes.append(field_data)
+            elif field_config["type"] == "list_text":
+                if d7fieldname in config["list_text_to_boolean"]:
+                    attributes.append(
+                        str(row[f"{d7fieldname}_value"]).strip()
+                        == config["list_text_to_boolean"][d7fieldname]
+                    )
+                else:
+                    attributes.append(row[f"{d7fieldname}_value"])
+            elif field_config["type"] == "datetime":
+                attributes.append(
+                    row[f"{d7fieldname}_value"].strftime(
+                        config["date_format_mappings"][d7fieldname]
+                    )
+                )
+            elif field_config["type"] == "list_boolean":
+                attributes.append(bool(row[f"{d7fieldname}_value"]))
+            elif field_config["type"] == "taxonomy_term_reference":
+                relationship_data = {}
+                d9_taxonomy_term = tid_to_d9_taxonomy_term[row[f"{d7fieldname}_tid"]]
+                relationship_data["type"] = d9_taxonomy_term["data"]["type"]
+                relationship_data["id"] = d9_taxonomy_term["data"]["id"]
+                relationships.append(relationship_data)
+            else:
+                raise ValueError(
+                    f"Unexpected field type encountered: {field_config['type']}"
+                )
+        if attributes:
+            field["data"]["attributes"][d9fieldname] = attributes
+        if relationships:
+            field["data"]["relationships"][d9fieldname] = {"data": relationships}
     return field
 
 
-def clean_uri(uri, nid_to_d9node):
+def clean_uri(uri, nid_to_d9_node):
+    # Normalize all internal links
     if uri.startswith("node/"):
         uri = "https://library.carleton.ca/" + uri
+    if uri.startswith("/node/"):
+        uri = "https://library.carleton.ca" + uri
     if uri.startswith("https://library.carleton.ca/node/"):
         nid = uri[len("https://library.carleton.ca/node/") :]
+        if nid[-1] == "/":
+            nid = nid[:-1]
         try:
-            d9node = nid_to_d9node[int(nid)]
+            d9node = nid_to_d9_node[int(nid)]
         except KeyError:
-            raise
+            click.echo(f"WARNING: Unable to find D9 Node for {nid}.")
+            d9node = {"data": {"attributes": {"drupal_internal__nid": "fourohfour"}}}
         uri = "internal:/node/" + str(
             d9node["data"]["attributes"]["drupal_internal__nid"]
         )
+    # Ensure all links to proxy are https
     if uri.startswith("proxy.library.carleton.ca"):
         uri = "https://" + uri
+    # Use qurl param to proxy instead of url
+    if uri.startswith("https://proxy.library.carleton.ca/login?url="):
+        uri = uri.replace("url", "qurl", 1)
     return uri
 
 
-def post_node(node, bundle, target, targetusername, targetpassword):
-    url = target + "/node/" + bundle
+def clean_text(value, nid_to_d9_node):
+    # Pass any links through the clean_uri function
+    re.sub(
+        r'href="(?P<href>[^"]+)"',
+        lambda m: 'href="' + clean_uri(m.group("href"), nid_to_d9_node) + '"',
+        value,
+    )
+    return value
+
+
+def post(obj, entity, bundle, target, targetusername, targetpassword):
+    url = target + "/" + entity + "/" + bundle
     headers = {
         "Accept": "application/vnd.api+json",
         "Content-Type": "application/vnd.api+json",
@@ -205,20 +368,26 @@ def post_node(node, bundle, target, targetusername, targetpassword):
     resp = requests.post(
         url,
         headers=headers,
-        data=json.dumps(node),
+        data=json.dumps(obj),
         auth=(targetusername, targetpassword),
     )
     try:
         resp.raise_for_status()
-    except:
-        click.echo(pprint.pformat(node))
-        click.echo(pprint.pformat(resp.json()))
+    except requests.RequestException:
+        click.echo(pprint.pformat(obj))
+        try:
+            click.echo(pprint.pformat(resp.json()))
+        except json.decoder.JSONDecodeError:
+            click.echo(resp.text)
         raise
     return resp.json()
 
 
-def patch_field(field, bundle, target, targetusername, targetpassword):
-    url = target + "/node/" + bundle + "/" + field["data"]["id"]
+def patch(obj, entity, bundle, target, targetusername, targetpassword):
+    # If the attributes of the field are blank, don't bother POSTing
+    if not obj["data"]["attributes"] and not obj["data"]["relationships"]:
+        return
+    url = target + "/" + entity + "/" + bundle + "/" + obj["data"]["id"]
     headers = {
         "Accept": "application/vnd.api+json",
         "Content-Type": "application/vnd.api+json",
@@ -226,12 +395,15 @@ def patch_field(field, bundle, target, targetusername, targetpassword):
     resp = requests.patch(
         url,
         headers=headers,
-        data=json.dumps(field),
+        data=json.dumps(obj),
         auth=(targetusername, targetpassword),
     )
     try:
         resp.raise_for_status()
-    except:
-        click.echo(pprint.pformat(field))
-        click.echo(pprint.pformat(resp.json()))
+    except requests.RequestException:
+        click.echo(pprint.pformat(obj))
+        try:
+            click.echo(pprint.pformat(resp.json()))
+        except json.decoder.JSONDecodeError:
+            click.echo(resp.text)
         raise
